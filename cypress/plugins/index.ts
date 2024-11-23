26f478d71789a48684519218ef2d649ab4cfad35
Antony Mik <anmik13@gmail.com>
2024-11-23 23:18:00 +0200

    ..

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,8 +1,6 @@
 import { unlinkSync, existsSync } from 'node:fs'
 
 export function deleteFile(path: string) {
-  console.log('file exists:', existsSync(path));
-
   try { unlinkSync(path) }
   catch (e) {
     console.error(e)
