6be438d95a212bf92356e29f0d9113e85235faa1
Antony Mik <anmik13@gmail.com>
2024-11-23 21:23:21 +0200

    ..

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -38,7 +38,7 @@ describe('Test general tabs functionalities', () => {
 })
 
 describe('Test ToDo tab', () => {
-  if(!process.env.RUN_ON_CI)
+  if(process.env.NODE_ENV !== 'ci')
     before(() => cy.task('deleteFile', './.playground/server/db/todo.json'))
 
   beforeEach(() => cy.visit('/?tab=todo'))
@@ -104,7 +104,7 @@ describe('Test ToDo tab', () => {
 })
 
 describe('Test Reminder tab', () => {
-  if(!process.env.RUN_ON_CI)
+  if(process.env.NODE_ENV !== 'ci')
     before(() => cy.task('deleteFile', './.playground/server/db/reminder.json'))
 
   beforeEach(() => cy.visit('/?tab=reminder'))
